77f79f8ed37c395f961297ceb59a57dd4e4a94bc
z0nkee191 <199979286+z0nkee191@users.noreply.github.com>
2026-03-18 12:54:11 +0200

    h/w

--- js/main.js
+++ js/main.js
@@ -1,31 +1,129 @@
-// let a = 1;
-// const array1 = ['Mango', 'Poly', 'Ajax'];
-// const logItems = (arrayFunc) => {
-//     for (let i = 0; i < arrayFunc.length; i++) {
-//         console.log(`${a} - ${arrayFunc[i]}`);
-//         a++;
-//     }
-//     return;
-// }
-// console.log(logItems(array1));
+let a = 1;
+const array1 = ['Mango', 'Poly', 'Ajax'];
+const logItems = (arrayFunc) => {
+    for (let i = 0; i < arrayFunc.length; i++) {
+        console.log(`${a} - ${arrayFunc[i]}`);
+        a++;
+    }
+}
+console.log(logItems(array1));
 
-// function calculateEngravingPrice(message, pricePerWord) {
-//     const trimmedMessage = message.trim();
-//     if (trimmedMessage === '') {
-//         return 0;
-//     }
-//     let spaceCount = 0;
-//     for (let i = 0; i < trimmedMessage.length; i++) {
-//         if (trimmedMessage[i] === ' ') {
-//             spaceCount++;
-//         }
-//     }
-//     const wordCount = spaceCount + 1;
-//     const totalPrice = wordCount * pricePerWord;
-//     return totalPrice;
-// }
+const calculateEngravingPrice = (message, pricePerWord) => {
+    const trimmedMessage = message.trim();
+    if (trimmedMessage === '') {
+        return 0;
+    }
+    let spaceCount = 0;
+    for (let i = 0; i < trimmedMessage.length; i++) {
+        if (trimmedMessage[i] === ' ') {
+            spaceCount++;
+        }
+    }
+    const wordCount = spaceCount + 1;
+    const totalPrice = wordCount * pricePerWord;
+    return totalPrice;
+}
 
-// const userMessage = prompt(`enter message`);
-// const userPricePerWord = prompt(`enter price per word`);
-// const funcReturn = calculateEngravingPrice(userMessage, userPricePerWord);
-// console.log(funcReturn);
+const userMessage = prompt(`enter message`);
+const userPricePerWord = prompt(`enter price per word`);
+const funcReturn = calculateEngravingPrice(userMessage, userPricePerWord);
+console.log(funcReturn);
+
+
+const userString = prompt(`enter something with the gap`);
+const findLongestWord = (string) =>{
+    const stringSplited = string.split(" ");
+    let max = stringSplited[0].length; 
+    let maxNotLength = stringSplited[0];
+    for (let i = 1; i < stringSplited.length; i++) {
+        if (stringSplited[i].length > max) {
+            max = stringSplited[i].length;
+            maxNotLength = stringSplited[i];
+        }
+    }
+    console.log(maxNotLength);
+    return max;
+}
+console.log(findLongestWord(userString));
+
+
+const userInfo = prompt(`enter something with 40 elements`);
+const formatString = (string) => {
+    if (string.length <= 40) {
+        return string;
+    } else {
+        return string.slice(0, 40) + '...';
+    }
+}
+console.log(formatString(userInfo));
+
+const userStr = prompt(`enter something`).toLowerCase();
+const checkForSpam = (message) => {
+    const userStrArray = message.split(" ");
+    let check = false;
+    for (let i = 0; i < userStrArray.length; i++) {
+        if (userStrArray[i] === "spam" || userStrArray[i] === "sale") {
+            check = true;
+            break;
+        } else {
+            continue;
+        }
+    }
+    return check;
+}
+
+console.log(checkForSpam(userStr));
+
+const numbers = [];
+
+while (true) {
+    const input = Number(prompt(`enter number`));
+    if (input === null) {
+        break;
+    }
+    const number = Number(input);
+    
+    if (!isNaN(number)) {
+        numbers.push(number);
+    } else {
+        alert('будь ласка, введіть коректне число');
+    }
+}
+if (numbers.length > 0) {
+    let total = 0;
+    for (const number of numbers) {
+        total += number;
+    }
+    console.log(`загальна сума чисел дорівнює ${total}`);
+} else {
+    console.log(`масив порожній, немає чого сумувати`);
+}
+
+const logins = ['admin', '111', 'zonkee'];
+const userLogin = prompt(`enter unique login`); 
+
+function isLoginValid(login) {
+    let check = false;
+    if (login.length > 4 && login.length < 16) {
+        check = true;
+    } else {
+        check = false;
+    }
+    return check;
+}
+function isLoginUnique(allLogins, login) {
+    return !allLogins.includes(login);
+}
+function addLogin(allLogins, login) {
+    if (isLoginValid(login) === true) {
+        if (isLoginUnique(allLogins, login) === true) {
+            allLogins.push(login);
+            return 'Логін успішно доданий!'; 
+        } else {
+            return 'Такий логін уже використовується!';
+        }
+    } else {
+        return 'Помилка! Логін повинен бути від 4 до 16 символів'; 
+    }
+}
+console.log(addLogin(logins, userLogin));
